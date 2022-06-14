3d4ae2046671b5e280af2cd0b39b7e1f76576acd
Vladislav <git13vl@gmail.com>
2022-06-14 22:00:28 +0200

    Initial Commit

---
+++ TVapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVapp
+//
+//  Created by Vladislav Glovatskiy on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
